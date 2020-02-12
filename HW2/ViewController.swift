eb626c6752b9571e89e29b13559aa762b2503f03
Anton <anton.alymov87@gmail.com>
2020-02-12 19:58:57 +0400

    Initial Commit

---
+++ HW2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2
+//
+//  Created by Антон Алымов on 12.02.2020.
+//  Copyright © 2020 Anton Alymov Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
